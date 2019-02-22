0c461c902cffb6c006619fe4ffe6c749416bbc69
Johnson <johnson.jesurajan@live.in>
2019-02-22 15:29:39 -0500

    code change in HelloWorld.java

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -3,18 +3,19 @@ package example;
 public class HelloWorld {
 
   public void coveredByUnitTest() {
-    System.out.println("coveredByUnitTest1");
-    System.out.println("coveredByUnitTest2");
+    logger.log("coveredByUnitTest1");
+    logger.log("coveredByUnitTest2");
+    logger.log("My Message");
   }
 
   public void coveredByIntegrationTest() {
-    System.out.println("coveredByIntegrationTest1");
-    System.out.println("coveredByIntegrationTest2");
-    System.out.println("coveredByIntegrationTest3");
+    logger.log("coveredByIntegrationTest1");
+    logger.log("coveredByIntegrationTest2");
+    logger.log("coveredByIntegrationTest3");
   }
 
   public void notCovered() {
-    System.out.println("notCovered");
+    logger.log("notCovered");
   }
 
 }
